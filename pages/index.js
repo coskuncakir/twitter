68e388c75ab01eae4879d1951fb352c3785b947c
coskun cakir <csknckr@gmail.com>
2020-06-06 14:30:48 +0100

    initial

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "../components/button";
+
+function HomePage() {
+  return (
+    <>
+      <h1>Welcome to twitter</h1>
+      <Button>Storybook Button</Button>
+    </>
+  );
+}
+
+export default HomePage;
